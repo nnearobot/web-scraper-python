a636462cb70f4dabb2d95d6d3c1587d3215e9e37
Maksiutova Rimma <nnearobot@gmail.com>
2023-08-19 14:40:45 +0900

    fetch.py: archive all assets as well as html

--- fetch.py
+++ fetch.py
@@ -4,12 +4,28 @@ import sys
 import os
 import requests
 from datetime import datetime
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 
 STORAGE_DIR = os.path.join(sys.path[0], 'pages')
 
-# Fetches the page from the given url:
+
+### Saves the assets near the html file: ###
+
+def save_asset(url, save_path):
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+
+        with open(save_path, 'wb') as file:
+            for chunk in response.iter_content(8192):
+                file.write(chunk)
+    except requests.RequestException as e:
+        print(f"Error fetching {url}: {e}")
+
+
+### Fetches the page from the given url: ###
+
 def fetch_url(url):
     try:
         ### 1. Let's fetch the page's HTML ###
@@ -20,20 +36,57 @@ def fetch_url(url):
         # Parse the page using BeautifulSoup
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Get the number of links and images
+        # Get the number of links, images and scripts
         links_num = len(soup.find_all('a'))
         images_num = len(soup.find_all('img'))
+        script_num = len(soup.find_all('script'))
 
-        ### 2. Save the file to the disk ###
 
-        # Extract the domain name to name the file
+        ### 2. Save all the assets to the local disk ###
+
+        # Create a directory to save assets
         domain_name = urlparse(url).netloc
-        filename = f"{domain_name}.html"
-        save_path = os.path.join(STORAGE_DIR, filename)
+        assets_dir = os.path.join(STORAGE_DIR, domain_name)
+        os.makedirs(assets_dir, exist_ok=True)
 
-        # Save the content
-        with open(save_path, 'w', encoding='utf-8') as file:
-            file.write(response.text)
+        # List of tags and attributes to search for URLs to download
+        assets_tags = {
+            "img": ["src"],
+            "script": ["src"],
+            "link": ["href", "stylesheet"]
+        }
+        asset_count = 0
+        for tag, attr_list in assets_tags.items():
+            rel = attr_list[1] if len(attr_list) > 1 else None
+            for element in soup.find_all(tag, rel=rel):
+                asset_url = element.attrs.get(attr_list[0], "")
+                if asset_url:
+                    absolute_asset_url = urljoin(url, asset_url)
+                    asset_name = os.path.basename(urlparse(asset_url).path)
+                    # there are some links not to files but to the APIs like  https://js.stripe.com/v2/
+                    # we leave them as is because there no particular file
+                    if asset_name:
+                        # some different assets on the page may have the same name (the may locate in the different directories)
+                        # so for not rewriting the files we make a new asset_name by adding an asset_count to the file name:
+                        splitted_filename = os.path.splitext(asset_name)
+                        asset_name = "{}_{}{}".format(splitted_filename[0], asset_count, splitted_filename[1])
+                        asset_count += 1
+
+                        local_asset_path = os.path.join(assets_dir, asset_name)
+                        print(tag, asset_name, asset_url, local_asset_path)
+
+                        save_asset(absolute_asset_url, local_asset_path)
+
+                        # Update the attribute in the html to point to the local asset
+                        element[attr_list[0]] = os.path.join(domain_name, asset_name)
+
+
+        ### 3. Save the modified HTML content to the disk ###
+        filename = f"{domain_name}.html"
+        html_filename = os.path.join(STORAGE_DIR, filename)
+        with open(html_filename, 'w', encoding='utf-8') as file:
+            file.write(str(soup))
+
 
         # Log to the console
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -41,6 +94,7 @@ def fetch_url(url):
         print(f"  - Saved as {filename}")
         print(f"  - Links found on the page: {links_num}")
         print(f"  - Images found on the page: {images_num}")
+        print(f"  - Scripts found on the page: {script_num}")
 
     except requests.RequestException as e:
         print(f"Error fetching {url}: {e}")
