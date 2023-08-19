fe64281eaed7afc80ecd8ba0ba1adad2b696515f
Maksiutova Rimma <nnearobot@gmail.com>
2023-08-19 20:16:18 +0900

    fetch.py: some code improvements

--- fetch.py
+++ fetch.py
@@ -8,6 +8,11 @@ from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 
 STORAGE_DIR = os.path.join(sys.path[0], 'pages')
+ASSETS_TAGS = {
+    "img": "src",
+    "script": "src",
+    "link": "href"
+}
 
 
 ### Saves the assets near the html file: ###
@@ -30,7 +35,7 @@ def fetch_url(url):
         response = make_request(url)
 
         # Parse the page using BeautifulSoup
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'lxml')
 
         # Get the number of links, images and scripts
         links_num = len(soup.find_all('a'))
@@ -46,35 +51,41 @@ def fetch_url(url):
         os.makedirs(assets_dir, exist_ok=True)
 
         # List of tags and attributes to search for URLs to download
-        assets_tags = {
-            "img": ["src"],
-            "script": ["src"],
-            "link": ["href", "stylesheet"]
-        }
         asset_count = 0
-        for tag, attr_list in assets_tags.items():
-            rel = attr_list[1] if len(attr_list) > 1 else None
-            for element in soup.find_all(tag, rel=rel):
-                asset_url = element.attrs.get(attr_list[0], "")
-                if asset_url:
-                    absolute_asset_url = urljoin(url, asset_url)
-                    asset_name = os.path.basename(urlparse(asset_url).path)
-                    # there are some links not to files but to the APIs like  https://js.stripe.com/v2/
-                    # we leave them as is because there no particular file
-                    if asset_name:
-                        # some different assets on the page may have the same name (the may locate in the different directories)
-                        # so for not rewriting the files we make a new asset_name by adding an asset_count to the file name:
-                        splitted_filename = os.path.splitext(asset_name)
-                        asset_name = "{}_{}{}".format(splitted_filename[0], asset_count, splitted_filename[1])
-                        asset_count += 1
+        for tag, attr in ASSETS_TAGS.items():
+            for element in soup.find_all(tag):
 
-                        local_asset_path = os.path.join(assets_dir, asset_name)
-                        print(tag, asset_name, asset_url, local_asset_path)
+                # get a url of the asset:
+                asset_url = element.attrs.get(attr, "")
+                if not asset_url:
+                    continue
 
-                        save_asset(absolute_asset_url, local_asset_path)
+                absolute_asset_url = urljoin(url, asset_url)
+                asset_name = os.path.basename(urlparse(asset_url).path)
 
-                        # Update the attribute in the html to point to the local asset
-                        element[attr_list[0]] = os.path.join(domain_name, asset_name)
+                # there are some links not to files but to the APIs like  https://js.stripe.com/v2/
+                # we leave them as is because there no particular file
+                if not asset_name:
+                    continue
+                
+                # also if the asset doesn't have a file extension it is supposet to not be an asset
+                splitted_filename = os.path.splitext(asset_name)
+                if len(splitted_filename) < 2 or not splitted_filename[1]:
+                    continue
+
+                # some different assets on the page may have the same name (they may locate in the different directories)
+                # for preventing a rewriting the files we make a new asset_name by adding an asset_count to the file name:
+                asset_name = "{}_{}{}".format(splitted_filename[0], asset_count, splitted_filename[1])
+                asset_count += 1
+
+                local_asset_path = os.path.join(assets_dir, asset_name)
+
+                print(tag, asset_name, splitted_filename, asset_url, local_asset_path)
+
+                save_asset(absolute_asset_url, local_asset_path)
+
+                # Update the attribute in the html to point to the local asset
+                element[attr] = os.path.join(domain_name, asset_name)
 
 
         ### 3. Save the modified HTML content to the disk ###
@@ -83,14 +94,12 @@ def fetch_url(url):
         with open(html_filename, 'w', encoding='utf-8') as file:
             file.write(str(soup))
 
-
-        # Log to the console
-        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(f"Fetched {url} at {current_time}")
-        print(f"  - Saved as {filename}")
-        print(f"  - Links found on the page: {links_num}")
-        print(f"  - Images found on the page: {images_num}")
-        print(f"  - Scripts found on the page: {script_num}")
+        return {
+            'filename': filename,
+            'links_num': links_num,
+            'images_num': images_num,
+            'script_num': script_num
+        }
 
     except requests.RequestException as e:
         print(f"Error fetching {url}: {e}")
@@ -115,7 +124,15 @@ def main():
         sys.exit(1)
 
     for url in sys.argv[1:]:
-        fetch_url(url)
+        print(f"Fetching {url}...")
+        res = fetch_url(url)
+        # Log to the console
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"Fetched {url} at {current_time}")
+        print(f"  - Saved as {res['filename']}")
+        print(f"  - Links found on the page: {res['links_num']}")
+        print(f"  - Images found on the page: {res['images_num']}")
+        print(f"  - Scripts found on the page: {res['script_num']}")
 
 
 if __name__ == "__main__":
