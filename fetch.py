a9d8382af0cbd4d3ada15696a65cfc3fffd6f618
Maksiutova Rimma <nnearobot@gmail.com>
2023-08-19 21:18:25 +0900

    fetcher: make a class Fetcher for future improvements

--- fetch.py
+++ fetch.py
@@ -7,119 +7,134 @@ from datetime import datetime
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 
-STORAGE_DIR = os.path.join(sys.path[0], 'pages')
-ASSETS_TAGS = {
-    "img": "src",
-    "script": "src",
-    "link": "href"
-}
+class Fetcher:
+    ASSETS_TAGS = {
+        "img": "src",
+        "script": "src",
+        "link": "href"
+    }
+
+    def __init__(self, url, save_dir=None):
+        self.url = url
+
+        # for testing purposes we let to define a directory to save a page:
+        self.save_dir = save_dir if save_dir else os.path.join(sys.path[0], 'pages')
+
+        self.domain_name = urlparse(url).netloc
+        self.filename = f"{self.domain_name}.html"
+        self.links_num = 0
+        self.images_num = 0
+        self.script_num = 0
+        
+
+    ### Saves the assets near the html file: ###
+    def save_asset(self, url, save_path):
+        try:
+            response = self.make_request(url, True)
+            with open(save_path, 'wb') as file:
+                for chunk in response.iter_content(8192):
+                    file.write(chunk)
+        except requests.RequestException as e:
+            print(f"Error fetching {url}: {e}")
+        except TypeError as e:
+            print(f"Error writing to a file {url}: {e}")
+            with open(save_path, 'wt') as file:
+                file.write('')
 
 
-### Saves the assets near the html file: ###
+    ### Fetches the page from the given url: ###
+    def fetch(self):
+        try:
+            ### 1. Let's fetch the page's HTML ###
+            response = self.make_request(self.url)
 
-def save_asset(url, save_path):
-    try:
-        response = make_request(url, True)
-        with open(save_path, 'wb') as file:
-            for chunk in response.iter_content(8192):
-                file.write(chunk)
-    except requests.RequestException as e:
-        print(f"Error fetching {url}: {e}")
-    except TypeError as e:
-        print(f"Error writing to a file {url}: {e}")
-        with open(save_path, 'wt') as file:
-            file.write('')
+            # Parse the page using BeautifulSoup
+            soup = BeautifulSoup(response.text, 'lxml')
 
+            # Get the number of links, images and scripts
+            self.links_num = len(soup.find_all('a'))
+            self.images_num = len(soup.find_all('img'))
+            self.script_num = len(soup.find_all('script'))
 
-### Fetches the page from the given url: ###
+            ### 2. Save all the assets to the local disk ###
+            # Create a directory to save assets
+            assets_dir = os.path.join(self.save_dir, self.domain_name)
+            os.makedirs(assets_dir, exist_ok=True)
 
-def fetch_url(url):
-    try:
-        ### 1. Let's fetch the page's HTML ###
-        response = make_request(url)
+            # List of tags and attributes to search for URLs to download
+            asset_count = 0
+            for tag, attr in self.ASSETS_TAGS.items():
+                for element in soup.find_all(tag):
 
-        # Parse the page using BeautifulSoup
-        soup = BeautifulSoup(response.text, 'lxml')
+                    # get a url of the asset:
+                    asset_url = element.attrs.get(attr, "")
+                    if not asset_url:
+                        continue
 
-        # Get the number of links, images and scripts
-        links_num = len(soup.find_all('a'))
-        images_num = len(soup.find_all('img'))
-        script_num = len(soup.find_all('script'))
+                    absolute_asset_url = urljoin(self.url, asset_url)
+                    asset_name = os.path.basename(urlparse(asset_url).path)
 
+                    # there are some links not to files but to the APIs like  https://js.stripe.com/v2/
+                    # we leave them as is because there no particular file
+                    if not asset_name:
+                        continue
+                    
+                    # also if the asset doesn't have a file extension, it is probably not an asset
+                    splitted_filename = os.path.splitext(asset_name)
+                    if len(splitted_filename) < 2 or not splitted_filename[1]:
+                        continue
 
-        ### 2. Save all the assets to the local disk ###
+                    # some different assets on the page may have the same file name (they may locate in a different directories)
+                    # for preventing a rewriting the files we make a new asset_name by adding an asset_count to the file name:
+                    asset_name = "{}_{}{}".format(splitted_filename[0], asset_count, splitted_filename[1])
+                    asset_count += 1
 
-        # Create a directory to save assets
-        domain_name = urlparse(url).netloc
-        assets_dir = os.path.join(STORAGE_DIR, domain_name)
-        os.makedirs(assets_dir, exist_ok=True)
+                    local_asset_path = os.path.join(assets_dir, asset_name)
+                    self.save_asset(absolute_asset_url, local_asset_path)
 
-        # List of tags and attributes to search for URLs to download
-        asset_count = 0
-        for tag, attr in ASSETS_TAGS.items():
-            for element in soup.find_all(tag):
+                    # Update the attribute in the html to point to the local asset
+                    element[attr] = os.path.join(self.domain_name, asset_name)
 
-                # get a url of the asset:
-                asset_url = element.attrs.get(attr, "")
-                if not asset_url:
-                    continue
-
-                absolute_asset_url = urljoin(url, asset_url)
-                asset_name = os.path.basename(urlparse(asset_url).path)
-
-                # there are some links not to files but to the APIs like  https://js.stripe.com/v2/
-                # we leave them as is because there no particular file
-                if not asset_name:
-                    continue
-                
-                # also if the asset doesn't have a file extension it is supposet to not be an asset
-                splitted_filename = os.path.splitext(asset_name)
-                if len(splitted_filename) < 2 or not splitted_filename[1]:
-                    continue
-
-                # some different assets on the page may have the same name (they may locate in the different directories)
-                # for preventing a rewriting the files we make a new asset_name by adding an asset_count to the file name:
-                asset_name = "{}_{}{}".format(splitted_filename[0], asset_count, splitted_filename[1])
-                asset_count += 1
-
-                local_asset_path = os.path.join(assets_dir, asset_name)
-
-                print(tag, asset_name, splitted_filename, asset_url, local_asset_path)
-
-                save_asset(absolute_asset_url, local_asset_path)
-
-                # Update the attribute in the html to point to the local asset
-                element[attr] = os.path.join(domain_name, asset_name)
+                    print(tag, asset_name, splitted_filename, asset_url, local_asset_path)
 
 
-        ### 3. Save the modified HTML content to the disk ###
-        filename = f"{domain_name}.html"
-        html_filename = os.path.join(STORAGE_DIR, filename)
-        with open(html_filename, 'w', encoding='utf-8') as file:
-            file.write(str(soup))
+            ### 3. Save the modified HTML content to the disk ###
+            html_filename = os.path.join(self.save_dir, self.filename)
+            with open(html_filename, 'w', encoding='utf-8') as file:
+                file.write(str(soup))
 
+        except requests.RequestException as e:
+            print(f"Error fetching {self.url}: {e}")
+
+
+    def get_metadata(self):
         return {
-            'filename': filename,
-            'links_num': links_num,
-            'images_num': images_num,
-            'script_num': script_num
+            'filename': self.filename,
+            'links_num': self.links_num,
+            'images_num': self.images_num,
+            'script_num': self.script_num
         }
+    
+    def print_logs(self):
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"Fetched {self.url} at {current_time}")
+        print(f"  - Saved as {self.filename}")
+        print(f"  - Links found on the page: {self.links_num}")
+        print(f"  - Images found on the page: {self.images_num}")
+        print(f"  - Scripts found on the page: {self.script_num}")
 
-    except requests.RequestException as e:
-        print(f"Error fetching {url}: {e}")
 
-
-def make_request(url: str, stream: bool=False):
-    response = requests.get(url, stream=stream, headers={
-        'X-Requested-With': 'XMLHttpRequest',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-        'Accept': '*/*',
-        'Connection': 'keep-alive',
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache'
-    })
-    response.raise_for_status()
-    return response
+    def make_request(self, url: str, stream: bool=False):
+        response = requests.get(url, stream=stream, headers={
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+            'Accept': '*/*',
+            'Connection': 'keep-alive',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache'
+        })
+        response.raise_for_status()
+        return response
 
 
 def main():
@@ -129,14 +144,11 @@ def main():
 
     for url in sys.argv[1:]:
         print(f"Fetching {url}...")
-        res = fetch_url(url)
-        # Log to the console
-        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(f"Fetched {url} at {current_time}")
-        print(f"  - Saved as {res['filename']}")
-        print(f"  - Links found on the page: {res['links_num']}")
-        print(f"  - Images found on the page: {res['images_num']}")
-        print(f"  - Scripts found on the page: {res['script_num']}")
+        fetcher = Fetcher(url)
+        fetcher.fetch()
+        fetcher.print_logs()
+    
+    print("\nDONE\n") 
 
 
 if __name__ == "__main__":
--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -1,8 +1,9 @@
 import os
+import sys
 import unittest
 from unittest.mock import patch, Mock
 
-import fetch
+from fetch import Fetcher
 
 class TestFetcher(unittest.TestCase):
 
@@ -14,11 +15,14 @@ class TestFetcher(unittest.TestCase):
         mock_response.raise_for_status = Mock()
         mock_get.return_value = mock_response
 
-        res = fetch.fetch_url("http://example.com")
+        save_dir = os.path.join(sys.path[0], 'tests')
+
+        fetcher = Fetcher("http://example.com", save_dir)
+        fetcher.fetch()
 
         # Check if the output file exists
-        index_path = os.path.join(fetch.STORAGE_DIR, "example.com.html")
-        assets_path = os.path.join(fetch.STORAGE_DIR, "example.com")
+        index_path = os.path.join(save_dir, "example.com.html")
+        assets_path = os.path.join(save_dir, "example.com")
         
         # as all the assets are indexed with counter the only image in test page would be image_0.jpg:
         image_path = os.path.join(assets_path, "image_0.jpg")
@@ -26,6 +30,8 @@ class TestFetcher(unittest.TestCase):
         self.assertTrue(os.path.exists(index_path))
         self.assertTrue(os.path.exists(assets_path))
         self.assertTrue(os.path.exists(image_path))
+
+        res = fetcher.get_metadata()
 
         self.assertEqual(res['filename'], "example.com.html")
         self.assertEqual(res['links_num'], 1)
